91a14f4f586c9f162d2d4cabbb450ab4cd979ca2
Li NIng <lning@i-uac.com>
2020-12-20 21:38:44 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,10 +1,9 @@
 package org.devops
-def tools = new tools()
 //构建类型
 def Build(buildType,buildShell){
   def buildTools = ["mvn":"m2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
-  tools.PrintMes("当前选择的构建类型为 ${buildType}")
-  //println("当前选择的构建类型为 ${buildType}")
+  
+  println("当前选择的构建类型为 ${buildType}")
   buildHome = tool buildTools[buildType]
   
   sh "${buildHome}/bin/${buildType} ${buildShell}"
