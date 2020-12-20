f377833d47f1ea8f49dc5308936192088136f84b
Li NIng <lning@i-uac.com>
2020-12-20 21:42:28 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -6,6 +6,11 @@ def Build(buildType,buildShell){
   println("当前选择的构建类型为 ${buildType}")
   buildHome = tool buildTools[buildType]
   
+  if("${buildType}" == "npm"){
+    sh " export NODE_HOME=${buildHome} && export PATH=\$NODE_HOME/bin:\$PATH && ${buildHome}/bin/npm ${buildShell}"
+  }
+  
+  
   sh "${buildHome}/bin/${buildType} ${buildShell}"
   
 }
